a6e90f9dc5e56d2eef7996c18c72575259ed9104
burhancelikk <burhancelikk47@gmail.com>
2022-10-01 20:59:40 +0300

    pypy

---
+++ Python Temel/deneme.py
@@ -0,0 +1,25 @@
+a=2
+
+print(a)
+
+print("Bugün Akdeniz'den Çiftliköy'\e 40 dakikada gittim.")
+
+x=int(input("bir sayi giriniz: "))
+
+print(type(x))
+
+print(x+15)
+
+mesaj=input("Mesajı gir: ") # str örnek
+
+isim=input("isim giriniz: ") # str örnek
+
+"""
+çoklu
+yorum
+satırı
+"""
+
+limon=2
+sonuc=limon*3
+print(sonuc)
