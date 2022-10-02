eaabcb61e33842de1cbf7226f00b523daaa93679
burhancelikk <burhancelikk47@gmail.com>
2022-10-02 19:09:12 +0300

    py

--- Yeni Baslayanlar Icin Python - Ahmet Aksoy/Bolum-7/Ornek-1.py
+++ Yeni Baslayanlar Icin Python - Ahmet Aksoy/Bolum-7/Ornek-1.py
@@ -1,11 +1,11 @@
 import math
-a=1
+a=3
 b=2
 print("a=",a,"b=",b)
 if(a>b):
-    print(a,"değeri",b,"\ 'den büyüktür")
+    print(a,"değeri",b,"\'den büyüktür")
 if(a<b):
-    prit(a,"değeri",b,"\ 'den küçüktür")
+    print(a,"değeri",b,"\'den küçüktür")
 if(b>0):
     c=math.sqrt(4*a+b**2)
-    print("(4*a+b**2)\ 'nin karekökü",c,"\'dir0")
+    print("(4*a+b**2)\'nin karekökü",c,"\'dir")
