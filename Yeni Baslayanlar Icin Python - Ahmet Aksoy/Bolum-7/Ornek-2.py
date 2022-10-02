667173c334151c91d270e5d5d6b15706979cd4c1
burhancelikk <burhancelikk47@gmail.com>
2022-10-02 19:09:25 +0300

    py

---
+++ Yeni Baslayanlar Icin Python - Ahmet Aksoy/Bolum-7/Ornek-2.py
@@ -0,0 +1,11 @@
+adlar=["Mehmet","Ali","John","Veli"]
+
+for ad in adlar:
+    if ad=='Veli':
+        print("Arkadaşının adı",ad)
+    elif ad=='Veli':
+        print("Arkadaşının adı",ad)
+    elif ad=='John':
+        print("Arkadaşın",ad,"bir İngilizdir.")
+    else:
+        print(ad,"adında birini tanımıyorum.")
