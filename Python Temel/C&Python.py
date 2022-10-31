cafea7fa5ca9a7dddc29d3b14d0bfba071f339df
burhancelikk <burhancelikk47@gmail.com>
2022-10-31 21:47:41 +0300

    pypy

---
+++ Python Temel/C&Python.py
@@ -0,0 +1,19 @@
+'''
+C dilindeki karşılığı
+
+#include <stdio.h>
+
+int main(){
+    printf("Hi");
+    for(int i=0;i<10;i++){
+        printf("\ni:%d",i);
+    }
+    return 0;
+}
+
+Python Dilindeki Karşılığı
+'''
+
+print("Hi")
+for i in range(10):
+    print("i:",i)
---
+++ Python Temel/fonksiyon-return.py
@@ -0,0 +1,4 @@
+# def fonksiyon_adı(input):
+
+def square(x):
+    x*x
