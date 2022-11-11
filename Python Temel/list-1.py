49b40559c656099b8f91300880240f8f55afa29e
burhancelikk <burhancelikk47@gmail.com>
2022-11-12 01:38:28 +0300

    pypy

--- Python Temel/list-1.py
+++ Python Temel/list-1.py
@@ -6,7 +6,7 @@ notlar[:5]
 l=[1,2,"a","b",True,4.5,[1,2,3]]
 l[6]
 l[3:6]
-notlar[1]+=5
+notlar[1]+=5 # notlar[1] = notlar[1] + 5
 notlar
 k=[1,2,3,4,5,6]
 k[:3]=30,40
--- Python Temel/list-test.py
+++ Python Temel/list-test.py
@@ -7,3 +7,7 @@ l2=l
 l[0]=l[0]+5
 l
 l2
+
+txt="Hello world" # Başında ve sonunda hiç boşluk bırakmadan değeri döndürür
+x=txt.strip()
+print(x) 
