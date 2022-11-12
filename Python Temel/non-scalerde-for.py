4d6ed975fedf930a9f5dde8a6f77d336941aeb16
burhancelikk <burhancelikk47@gmail.com>
2022-11-12 19:47:09 +0300

    pypy

--- Python Temel/non-scalerde-for.py
+++ Python Temel/non-scalerde-for.py
@@ -1,3 +1,6 @@
+from ast import If
+
+
 n=int(input())
 arr=list(map(int,input().split())) # Input:1 3 4 6 8 9 6 Output:[1,3,4,6,8,9,6]
 
@@ -5,28 +8,9 @@ print(arr)
 A=sorted(arr[:n])
 print(A)
 
-
-
 x=0
 while(x==0):
     if 2<=n<=10:
-        if -100<=A[i]<=100:
+        if -100<=A[If]<=100:
 
-
-'''
-if 2<=n<=10:
-    for i in A[:n]:
-        if -100<=A[i]<=100:   
-    print(A)
-    A.sort()
-else:
-    n=int(input())
-    arr=map(int,input().split())
-    print(A)
-    for _ in range(n-1):
-        if A[A.index(max(A)-i)]<A[A.index(max(A))]:
-            print(A[A.index(max(A)-i)])    
-else:
-    n=int(input())
-    arr=map(int,input().split())
-'''
+#arr=map(int,input().split())
