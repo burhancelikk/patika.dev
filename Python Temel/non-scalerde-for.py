93157e73ab36526d4a0313e98858a52f4d066aa5
burhancelikk <burhancelikk47@gmail.com>
2022-11-12 19:45:42 +0300

    pypy

--- Python Temel/non-scalerde-for.py
+++ Python Temel/non-scalerde-for.py
@@ -16,22 +16,16 @@ while(x==0):
 '''
 if 2<=n<=10:
     for i in A[:n]:
-        if -100<=A[i]<=100:
-            
-        
+        if -100<=A[i]<=100:   
     print(A)
     A.sort()
 else:
     n=int(input())
     arr=map(int,input().split())
-'''
-
-'''
     print(A)
     for _ in range(n-1):
         if A[A.index(max(A)-i)]<A[A.index(max(A))]:
-            print(A[A.index(max(A)-i)])
-        
+            print(A[A.index(max(A)-i)])    
 else:
     n=int(input())
     arr=map(int,input().split())
